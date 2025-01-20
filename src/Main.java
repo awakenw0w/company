115cd9655d83fe041e2ac1543b9d2a9c213317cc
pedagog_denchik <danilbelousov228@gmail.com>
2025-01-20 15:39:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,7 +15,15 @@ public class Main {
         companies.add(new Company("Технологический техникум", 150, 500000));
 
         // удаление компании из массива
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите название компании, которое нужно удалить: ");
+        String deleteName = s.nextLine();
+        companies.remove(deleteName);
 
-
+        // Вывод информации о всех компаниях
+        System.out.println("Все компании: ");
+        for (Company company : companies) {
+            
+        }
     }
 }
