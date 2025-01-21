819aec40561808fcfa168298957a3ec3946d6ab8
alusep <alesya_uspenskaya@icloud.com>
2025-01-21 11:37:03 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,33 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // сканер для ввода
+        Scanner s = new Scanner(System.in);
 
         // динамечский массив для хранения экземпляров класса company (name, workers, salary)
-        List<Company> companies = new ArrayList<>();
+        List<company> companies = new ArrayList<>();
 
         //добавляем компании в массив
         // в скобках - элементы массива внутри которого есть название компании, колво сотрудников и зарплата
-        companies.add(new Company("Энергетический техникум", 200, 1000000.00));
-        companies.add(new Company("Политехнический техникум", 100, 300000));
-        companies.add(new Company("Технологический техникум", 150, 500000));
+        companies.add(new company("Энергетический техникум", 200, 1000000.00));
+        companies.add(new company("Политехнический техникум", 100, 300000));
+        companies.add(new company("Технологический техникум", 150, 500000));
 
+        // выводим информацию о всех компаниях:
+        for(company company : companies) {
+            System.out.println(company);
+        }
         // удаление компании из массива
-        Scanner s = new Scanner(System.in);
         System.out.println("Введите название компании, которое нужно удалить: ");
         String deleteName = s.nextLine();
         companies.remove(deleteName);
 
-        // Вывод информации о всех компаниях
-        System.out.println("Все компании: ");
-        for (Company company : companies) {
-            company.getName();
-            company.getWorkers();
-            company.getSalary();
-            System.out.println();
+        for(company company : companies) {
+            System.out.println(company);
         }
     }
 }
